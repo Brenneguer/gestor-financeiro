ec5b6933ef83ce50db374ef77720675725dbec3a
Brenneguer <wbrenneguer07@gmail.com>
2020-06-16 11:36:10 -0300

    criação do componente de login e toolbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
