8298148115a3486ab2eb64c7d7b80d29cd8d2623
Brenneguer <wbrenneguer07@gmail.com>
2020-06-17 02:58:08 -0300

    criar botao cancelarEdicao

--- src/app/usuario/usuario.module.ts
+++ src/app/usuario/usuario.module.ts
@@ -4,15 +4,20 @@ import { MaterialModule } from './../material/material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsuarioComponent } from './usuario.component';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
 
-
+const routes: Routes = [
+  { path: '/', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [UsuarioComponent],
   imports: [
     CommonModule,
     MaterialModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class UsuarioModule { }
